de9021bacd50cfc8d31319228ded4b2c7dfe18d1
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-14 15:13:52 +0000

    Modified app.js to use cors

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+// var logger = require("morgan");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger("dev"));
+app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -27,6 +32,8 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 not Found");
   }
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
