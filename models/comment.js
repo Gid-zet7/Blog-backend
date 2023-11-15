0807a2f74c6e5f6e56ba3747d41fb3ed5dc23146
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-15 03:25:25 +0000

    Added the default mongoose timestamp. This adds a created At and Updated At automatically

--- models/comment.js
+++ models/comment.js
@@ -1,28 +1,33 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const CommentSchema = new Schema({
-  date_created: {
-    type: Date,
-    default: Date.now(),
-  },
+const CommentSchema = new Schema(
+  {
+    date_created: {
+      type: Date,
+      default: Date.now(),
+    },
 
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
 
-  content: {
-    type: String,
-    required: true,
-  },
+    content: {
+      type: String,
+      required: true,
+    },
 
-  parent_comment_id: {
-    type: Schema.Types.ObjectId,
-    ref: "Comment",
-    required: false,
+    parent_comment_id: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+      required: false,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Comment", CommentSchema);
--- models/postmodel.js
+++ models/postmodel.js
@@ -2,47 +2,47 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const CommentSchema = require("./comment");
 
-const PostModel = new Schema({
-  title: {
-    type: String,
-    minLength: 3,
-    maxLength: 100,
-    required: true,
-  },
-
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-
-  image: {
-    url: String,
-    owner: String,
-    source: String,
-  },
-
-  content: {
-    type: String,
-    required: true,
-  },
-
-  comments: [
-    {
-      type: [CommentSchema],
-      required: false,
+const PostModel = new Schema(
+  {
+    title: {
+      type: String,
+      minLength: 3,
+      maxLength: 100,
+      required: true,
     },
-  ],
 
-  category: {
-    type: String,
-    required: true,
-  },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
 
-  date_posted: {
-    type: Date,
-    default: Date.now(),
+    image: {
+      url: String,
+      owner: String,
+      source: String,
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    comments: [
+      {
+        type: [CommentSchema],
+        required: false,
+      },
+    ],
+
+    category: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("BlogPost", PostModel);
