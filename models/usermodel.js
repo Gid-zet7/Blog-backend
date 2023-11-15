226765bbb60e061f135935db63c458d989bc985c
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-15 08:19:13 +0000

    Modified usermodel; removed salt and hash from my database and included only the hashed password

--- models/usermodel.js
+++ models/usermodel.js
@@ -2,56 +2,51 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-
-  first_name: {
-    type: String,
-    minLength: 3,
-  },
-
-  last_name: {
-    type: String,
-    minLength: 3,
-  },
-
-  email: {
-    type: String,
-    maxLength: 1000,
-    unique: true,
-    required: true,
-  },
-
-  roles: [
-    {
+const UserSchema = new Schema(
+  {
+    username: {
       type: String,
-      default: "User",
+      required: true,
+      unique: true,
     },
-  ],
 
-  timestamp: {
-    type: Date,
-    default: Date.now(),
-  },
+    first_name: {
+      type: String,
+      minLength: 3,
+    },
 
-  salt: {
-    type: String,
-    required: true,
-  },
+    last_name: {
+      type: String,
+      minLength: 3,
+    },
 
-  hash: {
-    type: String,
-    required: true,
-  },
+    email: {
+      type: String,
+      maxLength: 1000,
+      unique: true,
+      required: true,
+    },
 
-  active: {
-    type: Boolean,
-    default: true,
+    roles: [
+      {
+        type: String,
+        default: "User",
+      },
+    ],
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    active: {
+      type: Boolean,
+      default: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", UserSchema);
