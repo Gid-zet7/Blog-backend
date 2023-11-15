2ec07626ea9b80dc1f006f8c3d3cc7988a69f4a4
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-15 08:25:54 +0000

    Modified users routes to respond to client requets

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userController = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", userController.user_list);
+router.post("/", userController.user_create);
+router.patch("/", userController.user_update);
+router.get("/", userController.user_delete);
 
 module.exports = router;
