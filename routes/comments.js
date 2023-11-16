780a21848c00482672cd640f04faa93fcf16cda0
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-16 22:44:44 +0000

    Added comment router

---
+++ routes/comments.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const commentController = require("../controllers/commentController");
+
+/* GET users listing. */
+router.get("/list", commentController.get_comments);
+router.get("/", commentController.get_post_comments);
+router.post("/", commentController.comment_create);
+router.patch("/", commentController.comment_edit);
+router.delete("/", commentController.delete_comment);
+
+module.exports = router;
