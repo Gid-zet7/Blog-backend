9c2f1d94e60a4c0f6e6a26620c9c79955004d1d2
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-11-26 05:14:09 +0000

    Commented out the router.patch (might add this feature later)

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const commentController = require("../controllers/commentController");
 /* GET users listing. */
 router.get("/list", commentController.get_comments);
 router.post("/", commentController.comment_create);
-router.patch("/", commentController.comment_edit);
+// router.patch("/", commentController.comment_edit);
 router.delete("/", commentController.delete_comment);
 
 module.exports = router;
